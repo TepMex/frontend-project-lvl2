7ce531894b171e141bd9f646204c6cb783000dcc
Mikhail Semikolenov <mihawww@yandex.ru>
2020-12-21 20:01:50 +0600

    Step 2

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,9 +1,11 @@
 import { program } from 'commander';
 
 const runGenDiff = (argv) => {
-    program.description('Compares two configuration files and shows a difference.');
-    program.version('0.0.1');
-    program.parse(argv);
+    program.description('Compares two configuration files and shows a difference.')
+        .version('0.0.1')
+        .option('-f, --format [type]', 'output format')
+        .arguments('<filepath1> <filepath2>')
+        .parse(argv);
 }
 
 export default runGenDiff;
